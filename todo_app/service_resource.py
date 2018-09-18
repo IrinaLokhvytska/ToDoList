ce9f17cf6ada2e615ebf5b6560a1d74c05a7d337
Iryna Lokhvytska <ilokh@softserveinc.com>
2018-09-18 15:30:34 +0300

    add user class for the mongo db

--- todo_app/service_resource.py
+++ todo_app/service_resource.py
@@ -1,5 +1,6 @@
 from sanic.response import html
 from db.todo_lists import ToDoList
+from db.user import User
 
 from jinja2 import Environment, PackageLoader, select_autoescape
 
@@ -22,4 +23,18 @@ async def home(request: object) -> object:
     to_do_class = ToDoList(request.app.db)
     to_do_list = await to_do_class.find_all_lists()
     content = await render('home.html', title='Sanic', to_do_list=to_do_list)
+    return content
+
+
+async def registration(request: object) -> object:
+    """Asynchronous function for registration users"""
+    user_class = User(request.app.db)
+    data = {
+        'login': 'ilokh',
+        'firstname': 'Iryna',
+        'lastname': 'Lokhvytska'
+    }
+    await user_class.insert_user(data)
+    users = await user_class.find_all_users()
+    content = await render('user.html', title='Sanic', users=users)
     return content
